0cd38dbfa269197885c25797c92209fbef1bd512
Tahakz86 <talgat.kulzhanov@gmail.com>
2022-03-30 01:56:47 +0500

    main

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home() {
           {products.map(product => {
             return (
               <div key={product.id} className={styles.card}>
+              <img src={product.image} alt={`Preview of ${product.title}`} />
                 <h3>{ product.title }</h3>
                 <p>{ product.description }</p>
                 <p>${ product.price }</p>
@@ -35,30 +36,6 @@ export default function Home() {
               </div>
             );
           })}
-        </div>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
